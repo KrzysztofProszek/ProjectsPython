263aa5c5a739901f1abf59441d1064fba0339951
kproszek@gmail.com <kproszek@gmail.com>
2023-06-22 22:20:43 +0200

    Added urls

--- Django/01/blog/views.py
+++ Django/01/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 
+# model Django == Python class
+# QuerySet Django == obiekt klasy Pythona
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
