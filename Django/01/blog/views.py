edad4fd6781d2ef37902450e94c5e1c8e261e31d
kproszek@gmail.com <kproszek@gmail.com>
2023-06-27 20:57:45 +0200

    Added statics, details of posts,

--- Django/01/blog/views.py
+++ Django/01/blog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 
 # model Django == Python class
 # QuerySet Django == obiekt klasy Pythona
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
